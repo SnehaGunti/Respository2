f4dbd7df6d6a2dcb2be9956076eb6a5debf98248
SnehaGunti <guntisneha944@gmail.com>
2026-01-20 17:16:34 +0530

    web-app

--- pythondir/hello.py
+++ pythondir/hello.py
@@ -1,1 +1,13 @@
 print("Hello World!")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Sneha, this is your Python app deployed through Jenkins!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
